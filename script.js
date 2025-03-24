954b7d8f5f54f95aa872823ac5be0b93238ac80e
Devuh <david.walter.reichert@gmail.com>
2025-03-23 20:47:13 -0500

    Display score and announce winner when one player reaches 5 points

--- script.js
+++ script.js
@@ -13,32 +13,34 @@ function getComputerChoice() {
     }
 }
 
-/*
-function getHumanChoice(display = "Rock, Paper, Scissors?") {
-    let choice = prompt(display).toLowerCase();
-    if(choice === "rock" || choice === "paper" || choice === "scissors") {
-        choice = choice.charAt(0).toUpperCase() + choice.substring(1);
-        return choice;
-    } else {
-        return getHumanChoice("I'm sorry, that wasn't a valid option, please try again.");
-    }
-}
-*/
-
 const selection = document.querySelector(".buttons");
+const result = document.querySelector(".result");
+const score = document.querySelector(".score");
+
+let humanScore = 0;
+let computerScore = 0;
 
 function playRound(computerChoice, humanChoice) {
     if(computerChoice === humanChoice) {
-        console.log("It's a tie!");
-        return "tie";
+        result.textContent = "It's a tie!";
     } else if ((computerChoice === "Rock" && humanChoice === "Scissors") 
         || (computerChoice === "Paper" && humanChoice === "Rock") 
         || (computerChoice === "Scissors" && humanChoice === "Paper")) {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
-            return "loss";
+            result.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`;
+            computerScore++;
     } else {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
-        return "win";
+        result.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
+        humanScore++;
+    }
+    
+    if(humanScore == 5) {
+        score.textContent = `You won the game ${humanScore} - ${computerScore}!`;
+        humanScore = computerScore = 0;
+    } else if(computerScore == 5) { 
+        score.textContent = `You lost the game ${humanScore} - ${computerScore}.`;
+        humanScore = computerScore = 0;
+    } else {
+        score.textContent = `Human: ${humanScore} - Computer: ${computerScore}`;
     }
 }
 
